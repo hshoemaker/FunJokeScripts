7561c73cf510a8605336609fc2b18ba21c768b8e
Harry Shoemaker <shoemaker323@gmail.com>
2022-07-06 09:23:17 -0500

    adding zaksRandomNameGen.js
    Adding a joke "random" name generator

---
+++ zaksRandomNameGen.js
@@ -0,0 +1,11 @@
+const randomNames = [
+    "Bob Saget",
+    "John Stamos",
+    "Dean Winchester",
+    "Sam Winchester",
+    "Clark Griswold",
+    "Ellen Griswold"
+];
+
+let index = Math.floor(Math.random() * randomNames.length);
+console.log(randomNames[index]);
